24fce4b8cf2bb4dba7c290e8848e8fb6f37c694c
Rothcold <rothcold@gmail.com>
2024-01-08 16:09:44 +0800

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,20 +3,26 @@ if status is-interactive
 end
 
 function init
-	fish_vi_key_bindings
-	set -gx GOPATH $HOME/Workspaces/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g
-	alias govm=g
-	if type -q setzkbmap
-		alias swapctrl='setxkbmap -option "ctrl:nocaps"'
-		swapctrl
-	end
-	fenv source $HOME/.profile
+    fish_vi_key_bindings
+    set -gx GOPATH $HOME/Workspaces/go
+    set -gx GOROOT $HOME/.go
+    set -gx PATH $GOPATH/bin $PATH
+    # g-install: do NOT edit, see https://github.com/stefanmaric/g
+    alias govm=g
+    if type -q setzkbmap
+        alias swapctrl='setxkbmap -option "ctrl:nocaps"'
+        swapctrl
+    end
+    fenv source $HOME/.profile
 end
 
 function fish_greeting
-	neofetch
-	fortune -e firefly | cowsay -f koala -W 80
-end 
+    neofetch
+    fortune -e song100-hant tang300-hant
+end
 
+function command_not_found
+    pkgfile $1
+end
 
 init
