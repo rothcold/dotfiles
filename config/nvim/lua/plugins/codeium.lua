5da6fbd1f36670c63f6bf6b70551aa30b8d49465
Fang Zhou <rothcold@gmail.com>
2025-02-20 19:56:22 +0800

    Update

--- config/nvim/lua/plugins/codeium.lua
+++ config/nvim/lua/plugins/codeium.lua
@@ -1,8 +1,9 @@
-local result = {}
-if not vim.g.vscode then
-  result = {
-    "Exafunction/codeium.vim",
-    event = "BufEnter",
-  }
-end
-return result
+-- local result = {}
+-- if not vim.g.vscode then
+--   result = {
+--     "Exafunction/codeium.vim",
+--     event = "BufEnter",
+--   }
+-- end
+-- return result
+return {}
