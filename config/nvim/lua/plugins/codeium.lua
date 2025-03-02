4cdf19e70f427398ceb8e304c03aeab5033ddb01
Rothcold <rothcold@gmail.com>
2025-03-02 20:54:09 +0800

    Upgrade lazyvim to 8 and remove avante

--- config/nvim/lua/plugins/codeium.lua
+++ config/nvim/lua/plugins/codeium.lua
@@ -1,9 +1,8 @@
--- local result = {}
--- if not vim.g.vscode then
---   result = {
---     "Exafunction/codeium.vim",
---     event = "BufEnter",
---   }
--- end
--- return result
-return {}
+local result = {}
+if not vim.g.vscode then
+  result = {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+  }
+end
+return result
