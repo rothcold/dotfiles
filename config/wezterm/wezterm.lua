abc3146e138f0ffe974cef8ad4548fd044778670
Rothcold <rothcold@gmail.com>
2025-06-27 20:10:22 +0800

    Using wezterm as default terminal in sway

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,13 +1,12 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-
-config.font = wezterm.font('Server Mono')
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-config.color_scheme = 'Tomorrow'
+config.font = wezterm.font("Server Mono")
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.color_scheme = "Catppuccin Mocha"
 
 config.hide_tab_bar_if_only_one_tab = true
 
