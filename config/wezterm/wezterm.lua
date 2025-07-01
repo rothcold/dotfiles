32f549d0d93a2d5776328728e715ecc621b2aa42
Rothcold <rothcold@gmail.com>
2025-07-01 17:44:41 +0800

    Add hypr configs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,11 +4,13 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Server Mono")
+config.font = wezterm.font("Maple Mono")
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.color_scheme = "Catppuccin Mocha"
 
 config.hide_tab_bar_if_only_one_tab = true
+
+config.enable_wayland = false
 
 -- and finally, return the configuration to wezterm
 return config
